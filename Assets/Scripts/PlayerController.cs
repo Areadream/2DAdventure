568ac5c3e49462443abbf89974f98cab97a756b2
Areadream <1252032060@qq.com>
2023-04-14 17:32:40 +0800

    实现PlayController

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //2023Äê4ÔÂ14ÈÕ17:32:09
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
