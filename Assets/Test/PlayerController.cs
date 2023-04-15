e311daf62d75be0bcbea93a25307bea188d58bbc
Areadream <1252032060@qq.com>
2023-04-15 20:38:03 +0800

    Update PlayerController.cs

--- Assets/Test/PlayerController.cs
+++ Assets/Test/PlayerController.cs
@@ -7,7 +7,7 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        print($"Hello уехЩ");
+        print($"Hello О©╫О©╫О©╫О©╫");
     }
 
     // Update is called once per frame
